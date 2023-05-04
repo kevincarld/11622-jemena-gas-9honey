d5bfa642b9e0817c42a50b179697bb25abfc50d6
Kevin David <kevincarld@gmail.com>
2023-05-04 11:08:39 +1000

    Meta updated

--- next.config.js
+++ next.config.js
@@ -18,21 +18,21 @@ const nextConfig = {
   },
   env: {
     appMeta: {
-      canonical: 'canonical in next.config.js', // https://brandedcontent.[masthead].com.au/xxx/xxx"
-      brand: 'brand in next.config.js', // e.g Black Hawk
-      title: 'title in next.config.js', // e.g Being a good pet parent starts at mealtime
-      description: 'description in next.config.js',
-      siteName: 'siteName Title in next.config.js', // The Sydney Morning Herald, Good Food
-      icon: 'smh', // smh, goodfood, nine, afr, traveller
-      themeColour: '#0a1633', // hex
-      ogImage: 'https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2022/12/lecreuset-social-media-bg-1.jpg',
-      date: 'dd-mm-yy', // e.g 03-12-23
-      year: 'yyyy' // e.g 2023
+      canonical: 'https://brandedcontent.nine.com.au/jemena/make-dream-home-dreamier', // https://brandedcontent.[masthead].com.au/xxx/xxx"
+      brand: 'Jemena', // e.g Black Hawk
+      title: 'Make your dream home even dreamier', // e.g Being a good pet parent starts at mealtime
+      description: 'Home is where the heart is, sure. But when we think about the beating heart of a home, we often overlook the elements that make it operate at its dreamiest.',
+      siteName: '9Honey', // The Sydney Morning Herald, Good Food
+      icon: 'honey', // smh, goodfood, nine, afr, traveller, brisbanetimes, honey
+      themeColour: '#26BCD7', // hex
+      ogImage: 'https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2023/05/11622_Jemena_Social.png',
+      date: '08-05-23', // e.g 03-12-23
+      year: '2023' // e.g 2023
     },
-    jobCode: '11111-name', // used for nine ga scripts
+    jobCode: '11622-jemena-gas', // used for nine ga scripts
     guidelines: true, // shows or hides layout guideline bars
     activateMasthead: false, // shows or hides masthead
-    mastheadOverride: false, // overrides masthead that's pulling from hostname: e.g honey,style,travel,kitchen,you,parenting,coach,pets,celebrity
+    mastheadOverride: 'honey', // overrides masthead that's pulling from hostname: e.g honey,style,travel,kitchen,you,parenting,coach,pets,celebrity
   }
 }
 
