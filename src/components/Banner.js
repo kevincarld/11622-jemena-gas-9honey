21dfb7f58830c362d16b2ee2a37e0adb53f0760b
Kevin David <kevincarld@gmail.com>
2023-05-08 10:36:36 +1000

    banner images

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,9 +11,9 @@ import { motion } from 'framer-motion'
 export default function Banner() {
 
   return (
-    <Figure>
-      <Img display={{base: 'block', lg: 'none'}} dimension='375x600' w='full' alt='living room' mock='banner-1' />
-      <Img display={{base: 'none', lg: 'block'}} dimension='1920x838' w='full' alt='living room' mock='banner-1' />
+    <Figure mt={{base:"30px", lg:'60px', d: '100px'}}>
+      <Img display={{base: 'block', lg: 'none'}} dimension='375x600' src={'./images/banner-sm.jpg'} w='full' alt='living room' mock='banner-1' />
+      <Img display={{base: 'none', lg: 'block'}} dimension='1920x838' src={'./images/banner.jpg'} w='full' alt='living room' mock='banner-1' />
     </Figure>
   )
 }
